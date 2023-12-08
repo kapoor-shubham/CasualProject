63db773f2c815dce8dba0e6d8da53e8e370b1fb4
Shubham Kapoor <skapoor@falabella.cl>
2023-12-08 21:59:12 +0530

    Initial Commit

---
+++ CasualProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CasualProject
+//
+//  Created by skapoor on 08/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
